0f810dc3b2972cd70cb367f6f5dc99616db12b79
lelis-devops <lelis.engineer@yahoo.com>
2025-08-09 16:21:48 -0300

    IAM.sh

---
+++ IAM.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+create_users () {
+
+declare -A user
+
+user[name]=Dev1
+user[password]="D@ni55cssmile22"
+echo "User for people: ${user[name]}"
+echo "User for password: ${user[password]}"
+echo "Keys for array: ${!user[@]}"
+
+aws iam create-user --user-name "${user[name]}"
+
+aws iam create-login --user "${user[name]}" --pasword "${user[password]}" --password-reset-required
+
+echo login tempory created for "${user[name]}"
+}
+
+create_users () {
+
